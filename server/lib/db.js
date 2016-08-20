4f3539efc841bfbb8388a3631b29c96e0a834b3b
saguaroStickup <mindlessjockabird@gmail.com>
2016-08-19 20:27:47 -0500

    added models

--- server/lib/db.js
+++ server/lib/db.js
@@ -1,10 +1,11 @@
 // Using promised-mongo:
-var pmongo = require('promised-mongo');
+const pmongo = require('promised-mongo');
 // This is preemptive, if we are in production, we want to use heroku's db address. otherwise, lets use our local campaign_test
-var dbName = process.env.NODE_ENV === 'production' ? process.env.MONGOLAB_URI : 'campaign_test';
+const dbName = process.env.NODE_ENV === 'production' ? process.env.MONGOLAB_URI : 'campaign_test';
 // Establish db
-var db = pmongo(dbName);
-var _ = require('lodash');
+const db = pmongo(dbName);
+const _ = require('lodash');
+const fixtures = require('./sample.js');
 
 // Delete everything function to clean database
 db.deleteEverything = function () {
@@ -20,4 +21,27 @@ db.deleteEverything = function () {
   }
 }
 
-module.exports = db; 
+db.getMongoID = function(idString) {
+  return pmongo.ObjectId(idString);
+}
+
+db.loadSample = function(records, collectionName) {
+  // Get the fixture from a file, this just an object
+  // where the key is the collection name and the
+  // value is an array of records to be inserted
+
+  _.each(fixtures, function(records, collectionName){
+    db.collection(collectionName).find().then(function(items){
+      // only insert if the collection is empty
+      if(items.length === 0) {
+        db.collection(collectionName).insert(records);
+      }
+    })
+  });
+}
+
+// Loading samples in a database, but not yet:
+// db.loadSample(fixtures, 'Users');
+
+
+module.exports = db;
