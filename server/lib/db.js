070e9dcdb1eee1492bae38a98b4c4ddc14a77867
taylor dragoo <taylor.dragoo@gmail.com>
2016-08-24 13:54:39 -0500

    removed mongoose from reqs

--- server/lib/db.js
+++ server/lib/db.js
@@ -1,13 +1,10 @@
 // Using promised-mongo:
 const pmongo = require('promised-mongo');
 const _ = require('lodash');
-const mongoose = require('mongoose');
-
 
 // Establish db
 const dbName = process.env.NODE_ENV === 'production' ? process.env.MONGOLAB_URI : 'mongodb://localhost:27017/campaign_test';
 const db = pmongo(dbName, ['users', 'characters']);
-mongoose.connect(dbName, options);
 
 // Delete everything function to clean database
 db.deleteEverything = function () {
