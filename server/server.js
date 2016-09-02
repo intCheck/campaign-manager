c0b378f702fe43ed1ab46741690c8bc58d82028c
gredfearn <grantredfearn@gmail.com>
2016-09-02 18:00:18 -0500

    'working'

--- server/server.js
+++ server/server.js
@@ -1,11 +1,30 @@
+'use strict';
+// Dependancies
 const restify = require('restify');
-users = require('./users');
-port = 3000;
+const chalk = require('chalk')
 
-var server = restify.createServer({
-	name: 'Restify Server'
-});
+/*
+* Server configurations
+*/
+// the infamous 'leet' port ヽ(ﾟｰﾟ*ヽ)ヽ(*ﾟｰﾟ*)ﾉ(ﾉ*ﾟｰﾟ)ﾉ cuz we elite.
+const port = process.env.PORT || 1337;
+// Better to set options seperate, incase you need to change later
+const serverOptions = {
+  name: 'Campagin_Manager',
+  version: '1.0.0'
+};
+const server = restify.createServer(serverOptions);
 
+/**
+* Middleware functions to help retrieving data from body or queries
+*/
+server.use(restify.bodyParser());
+server.use(restify.queryParser());
+
+
+/**
+* Default Route to test our server
+*/
 server.get('/', function(req, res, next) {
 	res.send('hello world')
 	return next();
