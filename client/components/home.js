96cc48bfc8d20d4fcfedd4b9b28d8912fb6b3822
saguaroStickup <mindlessjockabird@gmail.com>
2016-09-03 16:49:30 -0500

    GET request working

--- client/components/home.js
+++ client/components/home.js
@@ -1,9 +1,79 @@
-var m = require('mithril')
+const m = require('mithril');
+const request = require('superagent');
+const Req = require('../models/requests');
+// const Promise = require('bluebird');
 
-exports.controller = function (options) {}
+//Main Request Example creator controller
+const Home = module.exports;
+Home.controller = function () {
+  var ctrl = this;
+  ctrl.url = m.prop(null);
+  ctrl.response = m.prop('');
 
-exports.view = function (ctrl, options) {
-  return m('.my-component', [
-    m('h2', 'HELLO WORLD')
-  ])
+  ctrl.getReq = function (url) {
+    m.startComputation()
+
+    return new Promise(function(resolve, reject) {
+      return resolve(Req.getRequest(url))
+    })
+    .then(function(response) {
+      console.log('Response', response.text);
+      ctrl.response(response.text);
+      m.endComputation()
+    });
+
+
+  }
+
+  ctrl.postReq = Req.postRequest;
+
 }
+
+//Main view composed of all sub views in desired order of appearance
+Home.view = function (ctrl, options) {
+
+    var view = m('div', [
+      header(ctrl),
+      field(ctrl),
+      buttons(ctrl)
+    ]);
+
+  return view;
+}
+
+
+//A header to come before the steps:
+const header = function() {
+  return m('div', [
+    m('h3', 'Welcome to the Campaign Manager'),
+    m('p', 'To test the different server routes, simply enter in the route and click the button')
+  ])
+};
+
+const field = function(ctrl) {
+  return m('div', [
+    m('a', 'http:/localhost:1337/'),
+    m('input', {
+      type: 'text',
+      placeholder: "url",
+      oninput: function(e) {
+        e.preventDefault;
+        ctrl.url(this.value); }
+    }),
+    m('br'),
+    m('br'),
+    m('a', 'Response from server: ' + ctrl.response()),
+  ])
+};
+
+
+const buttons = function(ctrl) {
+  return m('button', {
+        type: 'submit',
+        onclick: function(e){
+          e.preventDefault();
+          ctrl.getReq(ctrl.url());
+        }
+     }, 'GET')
+
+};
