49d53bcaf83ae50e53ba226b13ebb5d9e2369129
saguaroStickup <mindlessjockabird@gmail.com>
2016-09-04 11:29:52 -0500

    added delete and put

--- client/models/requests.js
+++ client/models/requests.js
@@ -21,15 +21,57 @@ Req.getRequest = url => {
 
 // A basic post formula
 Req.postRequest = (url, data) => {
-  return request
-    .post(url)
-    .send(data)
-    .then( response => {
+  return new Promise(function(resolve, reject){
+    request
+      .post(url)
+      .send(data)
+      .then( response => {
+        resolve(response);
       console.log('Got a response for server');
     })
     .catch( error => {
+      reject(error)
       console.log('Got an error:', error )
     })
+  })
+};
+
+// A basic put formula
+Req.putRequest = (url, data) => {
+  return new Promise(function(resolve, reject) {
+    request
+    .put(url)
+    .send({id: data})
+    .set('Accept', 'application/json')
+    .then( response => {
+      resolve(response)
+      console.log('response', response);
+      console.log('Got a response for server');
+    })
+    .catch( error => {
+      reject(error)
+      console.log('Got an error:', error )
+    })
+  })
+};
+
+// A basic delete formula
+Req.delRequest = (url, data) => {
+  return new Promise(function(resolve, reject) {
+    request
+    .delete(url)
+    .send({id: data})
+    .set('Accept', 'application/json')
+    .then( response => {
+      resolve(response)
+      console.log('response', response);
+      console.log('Got a response for server');
+    })
+    .catch( error => {
+      reject(error)
+      console.log('Got an error:', error )
+    })
+  })
 };
 
 module.exports = Req;
